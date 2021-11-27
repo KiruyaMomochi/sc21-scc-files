d11048f3cf30f29f4c58efd97f3cb8e89b99647a
KiruyaMomochi <65301509+KiruyaMomochi@users.noreply.github.com>
2021-11-27 09:34:44 +0900

    Add original license

--- cloud-init/rdma.py
+++ cloud-init/rdma.py
@@ -1,4 +1,12 @@
 # Code borrowed from https://github.com/Azure/WALinuxAgent
+# Copyright 2018 Microsoft Corporation
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
 
 import xml.dom.minidom as minidom
 import os
